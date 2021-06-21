08d8dc885d8da0173241bbe5c5a231754c97b398
Paulo Vitor <paulovitor2123@gmail.com>
2021-06-21 00:49:00 -0300

    Initial Layout css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export function Header() {
+  return (
+    <header>
+      <nav className={styles.container}>
+        <img src="/images/logo.svg" alt="logo" />
+        <img src="/images/spacetraveling.svg" alt="logo" />
+      </nav>
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+import { FaCalendar, FaUser } from 'react-icons/fa';
 
 interface Post {
   uid?: string;
@@ -24,13 +28,57 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(props: HomeProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <p className={styles.title}>Como utilizar Hooks</p>
+        <p className={styles.subtitle}>
+          Pensando em sincronização em vez de ciclos de vida.
+        </p>
+        <div className={styles.footer}>
+          <div>
+            <FaCalendar size={15} />
+            <p>15 Mar 2021</p>
+          </div>
+          <div>
+            <FaUser size={15} />
+            <p>Joseph Oliveira</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 // export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+//   const prismic = getPrismicClient();
+//   const postsResponse = await prismic.query(
+//     [
+//       Prismic.predicates.at('document.type', 'posts')
+//     ],
+//     {
+//       fetch: ['posts.title', 'posts.content'],
+//       pageSize: 5
+//     }
+//   );
 
-//   // TODO
+//   console.log(postsResponse);
+//   // const posts = postsResponse.results.map(post => {
+//   //   return {
+//   //     slug: post.uid,
+//   //     title: RichText.asText(post.data.title),
+//   //     excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+//   //     updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+//   //       day: '2-digit',
+//   //       month: 'long',
+//   //       year: 'numeric'
+//   //     })
+//   //   }
+//   // });
+
+//   return {
+//     props: [],
+//     revalidate: 60 * 60 * 24, //24 hours
+//   }
 // };
